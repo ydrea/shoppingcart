a8f70ae1ef5fbfaa34806cfd17d9e2516a590f3e
ydrea <andrija.wrld@gmail.com>
2021-05-25 11:38:26 +0200

    1st

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import {useQuery} from 'react-query';
+
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import Badge from '@material-ui/core/Badge';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+// import Item from './Item/Item';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        alo
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {QueryClient, QueryClientProvider} from 'react-query'
+
+const client = new QueryClient
 
 ReactDOM.render(
-  <React.StrictMode>
+  <QueryClientProvider client={client} >
     <App />
-  </React.StrictMode>,
+    </QueryClientProvider>
+  ,
   document.getElementById('root')
 );
 
