ba9021e924d99a1155203020afaed0851dbe7b20
ydrea <andrija.wrld@gmail.com>
2021-05-25 12:00:06 +0200

    api types

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,22 @@ import Grid from '@material-ui/core/Grid';
 import Badge from '@material-ui/core/Badge';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 // styles
+import {Wrapper} from './App.styles'
 
+export type CartItemType = {
+  id: number;
+  title: string;
+  desc: string;
+  price: number;
+  date: string;
+  categoty: string;
+  userId: number;
+  imageUrl: string
+  quantity: number
+}
+
+const getProducts = async () : Promise <CartItemType[]> => 
+await (await fetch ('https://fakestoreapi.com/products')).json();
 
 function App() {
   return (
